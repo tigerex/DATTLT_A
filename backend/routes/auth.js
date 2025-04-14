e03d5545ad04e914aa78be8c1fc941f08b118f8f
Minhhu <62420892+tigerex@users.noreply.github.com>
2025-04-14 07:47:55 +0700

    cookie works

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,70 +1,74 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const router = express.Router();
 const { MongoClient } = require('mongodb');
+const cookieParser = require('cookie-parser');
 
-const url = "mongodb+srv://adminM:Raccoon-1@usertest.1opu14d.mongodb.net/"
-const dbName = "User";
+const app = express();
+const dbName = "User"; // Thay đổi tên cơ sở dữ liệu nếu cần
+const collectionName = "User"; // Tên collection trong MongoDB
+const accessPassword = "Raccoon-1"; // Mật khẩu truy cập MongoDB
+const url = "mongodb+srv://adminM:"+accessPassword+"@usertest.1opu14d.mongodb.net/?retryWrites=true&w=majority&appName=UserTest";
 
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
-const db = client.db(dbName);
-const testCollection = db.collection('Test');
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true }); // Kết nối MongoDB
+const db = client.db(dbName); // Kết nối đến cơ sở dữ liệu
+const userCollection = db.collection(collectionName); // Tạo collection để lưu trữ người dùng
 
+const maxAge = 3 * 24 * 60 * 60; // Thời gian hết hạn token (3 ngày)
+const ACCESS_SECRET_TOKEN = "Lola"; // Mã bí mật để mã hóa token (nên thay đổi trong thực tế)
 
-// Đăng ký người dùng mới 
-router.post('/register', async (req, res) => {
-  const { role, userName, passWord, email, phone, displayName, age } = req.body;
-  
-  try {
-    // Kiểm tra xem người dùng đã tồn tại chưa
-    const user = await testCollection.findOne({ userName: userName });
-    if (user) {
-      return res.status(400).json({msg: 'Người dùng đã tồn tại'});
-    }
-
-    else{
-      const newUser = {
-        role,
-        userName,
-        passWord,
-        email,
-        phone,
-        displayName,
-        age,
-        createdAt: new Date()
-      };
-
-      // Thêm vào MongoDB
-      const result = await testCollection.insertOne(newUser);
-
-      res.status(201).json({ msg: 'Đăng ký thành công!!!', userId: result.insertedId });
-    }
-
-  } catch (error) {
-    res.status(500).json({ msg: 'Lỗi server', error });
-  }
-});
+// middleware để xử lý JSON và cookie
+app.use(express.json()); // Sử dụng express.json() để xử lý JSON trong request body
+app.use(cookieParser()); // Sử dụng cookie-parser để xử lý cookie
 
 // Đăng nhập người dùng
 router.post('/login', async (req, res) => {
-  const { userName, passWord } = req.body;
+  const { email, passWord } = req.body;
+
   try {
+    if(!email || !passWord) {
+      throw new Error("MISSINGDATA"); // Kiểm tra xem có thiếu thông tin không
+    }
     // Tìm người dùng trong cơ sở dữ liệu
-    const user = await testCollection.find({ userName: userName }).toArray();;
-    if (!user) {
-      return res.status(400).json({ msg: 'ERROR!!!Người dùng không tồn tại!!!' });
+    const user = await userCollection.find({ email: email }).toArray();
+
+    if (user.length === 0) {
+      throw new Error("NOFIND"); // Kieerm tra xem người dùng có tồn tại không
     }
 
     // Kiểm tra mật khẩu
-    if (user[0].passWord !== passWord) {
-      return res.status(400).json({ msg: 'ERROR!!!Mật khẩu không đúng!!!' });
+    if (!await bcrypt.compare(passWord, user[0].passWord)) {
+      throw new Error("WRONGPASS"); // Kiểm tra mật khẩu có đúng không
     }
+    const token = generateAccessToken(user[0]._id); // Tạo token cho người dùng
+    console.log(token);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 }); // Ghi cookie vào trình duyệt
+    res.status(200).json({ msg: 'Đăng nhập thành công!!!', user });
 
-    res.send({ msg: 'Đăng nhập thành công!!!', user });
   } catch (error) {
-    res.status(500).json({ msg: 'Lỗi server jj đó', error });
+    if (error.message === "MISSINGDATA") {
+      return res.status(400).json({ msg: 'ERROR!!!Sai thông tin đăng nhập!!!' });
+    } 
+    if (error.message === "NOFIND") {
+      return res.status(400).json({ msg: 'ERROR!!!Người dùng không tồn tại!!!' });
+    } 
+    if (error.message === "WRONGPASS") {
+      return res.status(400).json({ msg: 'ERROR!!!Mật khẩu không đúng!!!' });
+    } 
+    else {
+      return res.status(500).json({ msg: 'ERROR!!!Lỗi server jj đó ở khúc đăng nhập á!!!', error });
+    }
+    
   }
 });
+
+function generateAccessToken(user) {
+  console.log("user:", user);
+  console.log("Key:", ACCESS_SECRET_TOKEN);
+  console.log("maxAge:", maxAge);
+  return jwt.sign({user}, ACCESS_SECRET_TOKEN, { expiresIn: maxAge});
+}
 
 module.exports = router;
