9dafbcbee28d37f499920768b695e52300dffd64
Feng <115795150+SlBananaCandyth@users.noreply.github.com>
2025-04-28 14:14:27 +0700

    Save local changes before pull

--- frontend/lib/services/auth_service.dart
+++ frontend/lib/services/auth_service.dart
@@ -7,7 +7,7 @@ class AuthService {
   // Địa chỉ server của bạn
   // Thay thế 'yourServerIp' bằng địa chỉ IP hoặc tên miền của server
   static String yourServerIp = '10.106.16.81'; // Thay thế bằng địa chỉ IP
-  static String baseUrl = 'http://${yourServerIp}:5000/api';
+  static String baseUrl = 'http://$yourServerIp:5000/api';
 
   static Future<http.Response> getUserInfo() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
