4aee3fde18d6dd439418f9b3ff97bd11873bbf3b
Feng <115795150+SlBananaCandyth@users.noreply.github.com>
2025-05-19 14:16:43 +0700

    push thoi

--- frontend/lib/screens/test_records.dart
+++ frontend/lib/screens/test_records.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
-import 'dart:convert';
+// import 'dart:convert';
 import '../models/test_result.dart';
+import '../models/test_question.dart';
+import './home22.dart';
+import '../services/result.service.dart';
+import '../services/question_service.dart';
+import '../widgets/RecordReviewCard.dart';
 
 class TestRecords extends StatefulWidget {
-  const TestRecords({super.key});
+  final String userId;
+
+  const TestRecords({super.key, required this.userId});
 
   @override
   _TestRecordsState createState() => _TestRecordsState();
 }
 
 class _TestRecordsState extends State<TestRecords> {
+  List<Result> records =
+      []; //records và tempList đều để chứa các bài test đã làm
+  List<Result> tempList =
+      []; //tempList được tạo ra để sử dụng cho hàm applyFilter và applySort bên dưới
   String selectedLevel = 'All';
   final List<String> levels = ['All', 'Easy', 'Medium', 'Hard'];
 
@@ -24,10 +35,11 @@ class _TestRecordsState extends State<TestRecords> {
   // Load các bài test đã làm ngay khi người dùng vào trang test records
   Future<void> loadResults() async {
     try {
-      final results = await ResultService().fetchResults();
+      final results = await ResultService().fetchResults(widget.userId);
       setState(() {
         records = results; // records là List<Result>
-        tempList = results; //Lý do gán cho cả tempList là để hiển thị ban đầu là All
+        tempList =
+            results; //Lý do gán cho cả tempList là để hiển thị ban đầu là All
       });
     } catch (e) {
       print("Lỗi khi load kết quả: $e");
@@ -44,25 +56,66 @@ class _TestRecordsState extends State<TestRecords> {
   //Hàm để sort các bài test theo điểm tăng/giảm dần hoặc theo ngày
   void applySort() {
     if (selectedSortOption == 'score_asc') {
-      widget.records.sort((a, b) => a.score.compareTo(b.score));
+      tempList.sort((a, b) => a.score.compareTo(b.score));
     } else if (selectedSortOption == 'score_desc') {
-      widget.records.sort((a, b) => b.score.compareTo(a.score));
-    // } else if (selectedSortOption == 'date_asc') {
-    //   widget.records.sort((a, b) => a.date.compareTo(b.date));
-    // } else if (selectedSortOption == 'date_desc') {
-    //   widget.records.sort((a, b) => b.date.compareTo(a.date));
+      tempList.sort((a, b) => b.score.compareTo(a.score));
+    } else if (selectedSortOption == 'date_asc') {
+      tempList.sort((a, b) => a.date!.compareTo(b.date!));
+    } else if (selectedSortOption == 'date_desc') {
+      tempList.sort((a, b) => b.date!.compareTo(a.date!));
     }
   }
 
-  @override
-  void initState() {
-    super.initState();
+  void applyFilter() {
+    if (selectedLevel == 'All') {
+      tempList = records;
+    } else {
+      tempList =
+          records
+              .where(
+                (r) => r.level.toLowerCase() == selectedLevel.toLowerCase(),
+              )
+              .toList();
+    }
+  }
+
+  void showRecordDetail(BuildContext context, Result record) async {
+    final questionIds = record.questions.map((q) => q.questionId).toList();
+    final detailedQuestions = <TestQuestion>[];
+
+    for (var id in questionIds) {
+      final question = await QuestionService().fetchWithID(id);
+      detailedQuestions.add(question);
+    }
+
+    showDialog(
+      context: context,
+      builder:
+          (_) => Dialog(
+            child: SizedBox(
+              height: 400,
+              width: double.maxFinite,
+              child: ListView.builder(
+                padding: EdgeInsets.all(20),
+                itemCount: detailedQuestions.length,
+                itemBuilder: (context, index) {
+                  return RecordReviewCard(
+                    question: detailedQuestions[index],
+                    answer: record.questions[index],
+                    index: index,
+                  );
+                },
+              ),
+            ),
+          ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 40),
         width: MediaQuery.of(context).size.width,
         height: MediaQuery.of(context).size.height,
         decoration: const BoxDecoration(
@@ -76,14 +129,24 @@ class _TestRecordsState extends State<TestRecords> {
           ),
         ),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
           children: [
+            Align(
+              heightFactor: 1,
+              alignment: Alignment.topLeft,
+              child: IconButton(
+                icon: const Icon(Icons.arrow_back, color: Color(0xFFFFFAFA)),
+                onPressed: goToBack, //Mũi tên để lui lại mấy câu hỏi trước
+              ),
+            ),
             Row(
+              mainAxisSize: MainAxisSize.max,
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 Text(
                   'Quiz Time',
                   style: TextStyle(
-                    fontSize: 20,
+                    fontSize: 23,
                     fontWeight: FontWeight.bold,
                     color: Color(0xFFFFFAFA),
                   ),
@@ -164,23 +227,121 @@ class _TestRecordsState extends State<TestRecords> {
                 ),
               ],
             ),
-            const SizedBox(height: 15,),
+            const SizedBox(height: 15),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 13.0),
               child: Container(
-                decoration: BoxDecoration(),
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 15,
+                ),
+                height: 500,
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFFAFA),
+                  borderRadius: BorderRadius.circular(30),
+                ),
                 child: Center(
                   child: Column(
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Row(),
-                      // LinearBorder(),
-                      // list.generate results with values (level, score, date)
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              'Level',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Text(
+                              'Score',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 3,
+                            child: Text(
+                              'Date',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      // Body scrollable
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: tempList.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                const SizedBox(height: 20),
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Color(0xFFFFFAFA),
+                                    shadowColor: Colors.grey.shade400,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30),
+                                    ),
+                                    padding: const EdgeInsets.symmetric(
+                                      vertical: 20,
+                                    ),
+                                  ),
+                                  child: Row(
+                                    children: [
+                                      Expanded(
+                                        flex: 2,
+                                        child: Text(
+                                          tempList[index].level,
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 2,
+                                        child: Text(
+                                          '${tempList[index].score}/10',
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                      Expanded(
+                                        flex: 3,
+                                        child: Text(
+                                          tempList[index].formattedDateTime,
+                                          textAlign: TextAlign.center,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  onPressed:
+                                      () => showRecordDetail(
+                                        context,
+                                        tempList[index],
+                                      ),
+                                ),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ),
             ),
-            const SizedBox(height: 20),
           ],
         ),
       ),
